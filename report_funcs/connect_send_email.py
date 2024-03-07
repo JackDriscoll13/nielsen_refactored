eb54d24ce63dd10b49c137e65509dc438e9fcefa
Jack Driscoll <jadr2353@colorado.edu>
2024-03-07 00:51:38 -0500

    trying this

--- report_funcs/connect_send_email.py
+++ report_funcs/connect_send_email.py
@@ -1,20 +1,22 @@
 import smtplib
+import ssl
 import time
+
+
 
 def send_email(emails, email_to): 
     print('\tConnecting to outlook email server, Logging in with credentials ->', end = ' ' )
-    s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-    s.connect("smtp-mail.outlook.com",587)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login('sn_audience_insights@outlook.com', 'AudienceInsights1')
-    print('Done. Logged in.')
 
-    for msg in emails:
-        msg_subject = msg['subject']
-        print(f'\tSending email: {msg_subject} ->', end = ' ')
-        s.send_message(msg, 'sn_audience_insights@outlook.com', email_to)
-        time.sleep(12)
-        print('Done. Sent mail.')
-    s.quit()
+    context=ssl.create_default_context()
+    with smtplib.SMTP("smtp-mail.outlook.com", port=587) as s:
+
+        s.starttls(context=context)
+        s.login('sn_audience_insights@outlook.com', 'AudienceInsights1')
+        print('Done. Logged in.')
+
+        for msg in emails:
+            msg_subject = msg['subject']
+            print(f'\tSending email: {msg_subject} ->', end = ' ')
+            s.send_message(msg, 'sn_audience_insights@outlook.com', email_to)
+            time.sleep(12)
+            print('Done. Sent mail.')
