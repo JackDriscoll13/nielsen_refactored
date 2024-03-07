9ec743bf21c5c2dd5084a227a072d5b8e10c3288
Jack Driscoll <jadr2353@colorado.edu>
2024-03-07 00:28:19 -0500

    add a line

--- report_funcs/connect_send_email.py
+++ report_funcs/connect_send_email.py
@@ -5,6 +5,7 @@ def send_email(emails, email_to):
     print('\tConnecting to outlook email server, Logging in with credentials ->', end = ' ' )
     s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
     s.starttls()
+    s.ehlo_or_helo_if_needed()
     s.login('sn_audience_insights@outlook.com', 'AudienceInsights1')
     print('Done. Logged in.')
 
